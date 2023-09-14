31d1e145b6ce04a4f1bacd7f055898d60055ad9d
Jasmin1002 <jasmineolofin@gmail.com>
2023-09-14 12:28:46 +0100

    doc: Add application Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="logo flex items-center">
+      <img src="./newtv.png" alt="movieBox" />
+      <h3 className="ml-4 text-xl">MovieBox</h3>
+    </div>
+  );
+}
+
+export default Logo;
