af554c39d46185810582b7b3093a8ab00e790043
Jasmin1002 <jasmineolofin@gmail.com>
2023-09-14 12:28:11 +0100

    doc: Add application header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import NavBar from "./NavBar";
+import Logo from "./Logo";
+import SearchBox from "./SearchBox";
+import Menu from "./Menu";
+import MovieDetail from "./MovieDetail";
+
+// eslint-disable-next-line react/prop-types
+function Header({ query, onChange, header, navbar }) {
+  return (
+    <header
+      ref={header}
+      className="banner h-4/6 bg-[url('/Poster.png')] bg-cover bg-no-repeat text-white lg:px-20 py-5 pb-20 "
+    >
+      <NavBar navbar={navbar}>
+        <Logo />
+        <SearchBox query={query} onChange={onChange} />
+        <Menu />
+      </NavBar>
+      <MovieDetail />
+    </header>
+  );
+}
+
+export default Header;
