a07f4800ee55a101a4cb8a563bb9ee2bee91ca82
Jasmin1002 <jasmineolofin@gmail.com>
2023-09-14 12:30:47 +0100

    doc: Add application Menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+  return (
+    <div className="menu">
+      <button className="btn btn-flat">Sign In</button>
+      <span className="btn-menu">
+        <img src="/hamburger.png" alt="hamburger icon" className="hamburger" />
+      </span>
+    </div>
+  );
+}
+
+export default Menu;
